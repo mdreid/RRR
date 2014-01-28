4a4d341c823b07d89eb4cdbcd67bdebf56602520
Katherine Lee <kate.lee168@gmail.com>
2014-01-28 11:29:11 -0500

    new index.php file based on index.html created by Angelica

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8">
+	<meta name="A non-profit that aims to spread literacy and compassion by connecting communities through the power of storytelling." content = "Read Record Replay">
+	<title>Read Record Replay</title>
+	<link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<?php include("header.php") ?>
+	<div id = "main">
+	</div>
+</body>
+</html>
