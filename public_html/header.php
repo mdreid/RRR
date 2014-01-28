1e6c0e073f432c1bc27fbbf724abd6ca13f933f8
Katherine Lee <kate.lee168@gmail.com>
2014-01-28 11:28:36 -0500

    adding in new header.php (initial)

---
+++ public_html/header.php
@@ -0,0 +1,17 @@
+<body>
+	<div class = "header">
+		<img height="50" src="https://lh4.googleusercontent.com/q6VOUW3jBGW_9-hMGowqCtxtzP0YnvOVVqUfQ17uyFIGL_fqvF58OVKwFHlQTAqoNg=w1308-h594"/>
+	<div class = "menu">
+		<!--<img height="70" src="https://lh4.googleusercontent.com/q6VOUW3jBGW_9-hMGowqCtxtzP0YnvOVVqUfQ17uyFIGL_fqvF58OVKwFHlQTAqoNg=w1308-h594"/>-->
+		<ul>
+			<li><a>Contact Us</a>
+			<li><a>About</a>
+				<ul>
+					<li><a>Our Team</a></li>
+					<li><a>Mission</a></li>
+				</ul>
+			</li>
+			<li><a>Events</a></li>
+		</ul>
+	</div></div>
+</body>
